d05190cf21ce1e7029b40a988728600d439273bb
Andrew McDowell <madoliole@gmail.com>
2014-09-02 17:44:28 +1000

    first push

---
+++ client/js/main.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Madole on 1/09/2014.
+ */
+
+$(document).ready(function(){
+  $('#title').addClass('animated bounceInLeft');
+  $('#form').addClass('animated bounceInRight');
+  $('hr').addClass('animated fadeIn');
+  $('#footer').addClass('animated bounceInUp');
+});
+
+function getFucksFromServer(){
+  var result      = $('#result');
+  var user        = $('#user');
+  var username    = $("#username").val();
+  $('#form').addClass('animated hinge');
+  $('.spinner').show();
+  showTweetCycler();
+
+  $.getJSON('http://localhost:8000/getFucks?username='+ username +'', function(obj) {
+    var resultText = 'In the last <span>100</span> tweets, you have given <span>'+obj.count+'</span> fucks!';
+    user.text('@' + username);
+    $('.spinner').hide();
+    result.html(resultText);
+    result.addClass('animated fadeIn');
+    showTweetCycler(obj.tweets);
+
+    console.log('-------------')
+    console.log(obj);
+    console.log('-------------')
+  });
+}
+
+function showTweetCycler(tweets) {
+  var first = $('#cycler');
+  var second= $('#cycler2');
+  var cycleArr = [first, second];
+  var current = first;
+  setInterval(function() {
+    var previous = current;
+    current = cycleArr[getNext(current)];
+    previous.removeClass();
+    previous.addClass('animated flipOutX');
+    current.html(tweets[Math.floor(Math.random() * tweets.length)]);
+    current.removeClass();
+    current.addClass('animated flipInX');
+  }, 5000);
+}
+
+function getNext(current) {
+  var first = $('#cycler');
+  if(current.selector === first.selector) {
+    return 1
+  }else{
+    return 0;
+  }
+}
---
+++ server/router.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Madole on 1/09/2014.
+ */
+var express   = require('express'),
+    app       = express(),
+    Twit      = require('twit'),
+    moment    = require('moment'),
+    nl        = require('newlinetostring');
+
+var twit = new Twit({
+  consumer_key:'taSymqkQcArH8UpIoZsb6nwiZ',
+  consumer_secret: 'xUZBBUW3QBD9EyUw0NWO5b9FEbiJXqMOdFcU2ShjdhPpXTMLoP',
+  access_token: '20041121-agD5XfplN9pEcZ8xqHTXzd5aJixADqp3WwAHlML26',
+  access_token_secret: 'Dt8PM98hAM5sxn5D191S8DfQnJI0uWVV7Aj8dyNMHdH5R'
+});
+
+function getFucksFromTwitter(req, res) {
+  var query = req._parsedUrl.query.split('=');
+  if(!query[0] === 'username') {return;}
+  var search = '@' + query[1];
+  console.log(search)
+  twit.get('statuses/user_timeline', {screen_name: search, count: 100}, function(err, data) {
+    var retObj = {
+      count: 0,
+      tweets: []
+    }
+    if(!data) {
+      res.status(404);
+      res.send('Couldn`t get data');
+      return;
+    }
+    data.forEach(function(tweet){
+
+      if(tweet.text.toLowerCase().indexOf('fuck') !== -1) {
+
+        var tweetArr = tweet.text.split(' ');
+        tweetArr = tweetArr.map(function(word) {
+          if(word.indexOf('@')===0) {
+            word = '<span class="twitterHandle"><a href="http://twitter.com/'+word+'">'+word+'</a></span>';
+          }
+          return word;
+        });
+        tweet.text = tweetArr.join(' ');
+        retObj.tweets.push(nl(tweet.text + ' ' + moment(tweet.created_at).fromNow()));
+        retObj.count++;
+      }
+    });
+    console.log('Your percentage of fucks given in the last 100 tweets is: ' + retObj.count + '%');
+    res.status(200);
+    res.send(retObj);
+  });
+}
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get('/getFucks/', getFucksFromTwitter);
+
+var server = app.listen(8000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
+
